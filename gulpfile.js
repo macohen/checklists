c5c635381115985d42dc98c8982551ab38989168
Mark Cohen <mark.cohen@viacom.com>
2015-06-17 17:54:24 -0400

    removed gulp-develop because jenkins was choking on it.

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var concat = require('gulp-concat')
 var uglify = require('gulp-uglify')
 var ngAnnotate = require('gulp-ng-annotate')
 var sourceMaps = require('gulp-sourcemaps')
-var developServer = require('gulp-develop-server')
+//var developServer = require('gulp-develop-server')
 
 gulp.task('js', function() {
     gulp.src(['ng/module.js','ng/**/*.js'])
@@ -19,6 +19,6 @@ gulp.task('watch:js', ['js'], function() {
     gulp.watch('ng/**/*.js', ['js'])
 })
 
-gulp.task('server:restart', function() {
+/*gulp.task('server:restart', function() {
     gulp.watch(['./app.js'], developServer.restart);
-})
+})*/
